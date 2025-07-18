7508ce6aaab1ad18130c2d4fe3b6233e216c4c30
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-07-18 19:17:52 +0300

    create employer-dashboard component

---
+++ src/app/pages/employer-dashboard/employer-dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-employer-dashboard',
+  imports: [RouterModule],
+  templateUrl: './employer-dashboard.html',
+  styleUrl: './employer-dashboard.css'
+})
+export class EmployerDashboard {
+
+}
