7ef14f9c17c3f48cea67bf8d0d7258f8d21fd4d0
PavelAC <pavelandreicalin@gmail.com>
2025-07-23 15:46:46 +0300

    fix folder

--- src/app/pages/timesheet-page/timesheet-page.component.ts
+++ src/app/pages/timesheet-page/timesheet-page.component.ts
@@ -12,13 +12,13 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 
-import { TimesheetTableComponent } from '../components/timesheet-table/timesheet-table.component';
-import { AddModal } from '../components/modal/add-modal/add-modal';
-import { EditModal } from '../components/modal/edit-modal/edit-modal';
-import { TimeEntryService } from '../services/time-entry.service';
-import { TimeEntry } from '../interfaces/time-entry.interface';
+import { TimesheetTableComponent } from '../../components/timesheet-table/timesheet-table.component';
+import { AddModal } from '../../components/modal/add-modal/add-modal';
+import { EditModal } from '../../components/modal/edit-modal/edit-modal';
+import { TimeEntryService } from '../../services/time-entry.service';
+import { TimeEntry } from '../../interfaces/time-entry.interface';
 
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
